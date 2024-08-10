1b6ab15e0ecb053c02d8d8e94bba845151a43501
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-10 15:34:44 +0530

    Intrageted Shadcn-ui Successfully

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils"; 
+import { ThemeProvider } from "@/components/theme-provider";
 
-const fonts = Plus_Jakarta_Sans({ 
+const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-sans'
@@ -22,7 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialised')}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialised', fontSans.variable)}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-white text-3xl underline">
-        HOME
+        Hello
       </h1>
+      <Button>Click Me</Button>
+      
     </div>
   );
 }
