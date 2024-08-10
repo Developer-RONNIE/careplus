45fcc30f03f4a7c96434cd6673849ca6d71318d4
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-10 15:17:03 +0530

    Updated Project themes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-white text-3xl underline">
         HOME
       </h1>
     </div>
